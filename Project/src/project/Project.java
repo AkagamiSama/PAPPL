50a4dfb975e60e2dd265fdf182df402c63a6f90a
AkagamiSama <ben.verdier@hotmail.fr>
2014-12-01 21:55:08 +0100

    Création de l'interface graphique et ajout de la génération dans le terminal. TODO : génération dans un/plusieurs fichiers.

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -5,6 +5,8 @@
  */
 package project;
 
+import GUI.MainGUI;
+
 /**
  *
  * @author akagami
@@ -16,42 +18,41 @@ public class Project {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Production a = new Production("pute ");
-        Production b = new Production("pute ");
-        Production c = new Production("pute ");
-        
-        Production B = new Production("B");
-        Expression Be1 = new Expression(); 
-        Be1.addMot(b);
-        B.addExpr(Be1);
-        Expression Be2 = new Expression();
-        Be2.addMot(c);
-        B.addExpr(Be2);
-        
-        Production A = new Production("A");
-        Expression Ae1 = new Expression();
-        Ae1.addMot(a);
-        Ae1.addMot(B);
-        A.addExpr(Ae1);
-        Expression Ae2 = new Expression();
-        Ae2.addMot(a);
-        Ae2.addMot(A);
-        Ae2.addMot(A);
-        A.addExpr(Ae2);
-        
-        Production S = new Production("S");
-        Expression Se = new Expression();
-        Se.addMot(A);
-        Se.addMot(B);
-        S.addExpr(Se);
-        Grammar g = new Grammar(S);
-        
-        String test = g.generate();
-        
-        //System.out.println(test);
-        
-        Parser pars = new Parser("grammar.txt");
-        
-        System.out.println(pars.grammarReadBNF().generate());
+//        Production a = new Production("pute ");
+//        Production b = new Production("pute ");
+//        Production c = new Production("pute ");
+//        
+//        Production B = new Production("B");
+//        Expression Be1 = new Expression(); 
+//        Be1.addMot(b);
+//        B.addExpr(Be1);
+//        Expression Be2 = new Expression();
+//        Be2.addMot(c);
+//        B.addExpr(Be2);
+//        
+//        Production A = new Production("A");
+//        Expression Ae1 = new Expression();
+//        Ae1.addMot(a);
+//        Ae1.addMot(B);
+//        A.addExpr(Ae1);
+//        Expression Ae2 = new Expression();
+//        Ae2.addMot(a);
+//        Ae2.addMot(A);
+//        Ae2.addMot(A);
+//        A.addExpr(Ae2);
+//        
+//        Production S = new Production("S");
+//        Expression Se = new Expression();
+//        Se.addMot(A);
+//        Se.addMot(B);
+//        S.addExpr(Se);
+//        Grammar g = new Grammar(S);
+//        
+//        String test = g.generate();
+//        
+//        //System.out.println(test);
+//        
+          MainGUI window = new MainGUI();
+          window.setVisible(true);
     }
 }
